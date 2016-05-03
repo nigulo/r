4cabfde1285502cc29a36c7e50763b8c7dc1468f
nigulo <nigulolspert@hotmail.com>
2016-05-03 14:13:13 +0300

    Comparison with actual convolution of power spectrum

--- d2_limit.R
+++ d2_limit.R
@@ -18,28 +18,28 @@ if (testDataNo == 1) {
   timeSpan <- x[length(x)] - x[1]
 }
 sampling <- timeSpan/count
-cohlen <- 5
+cohlen <- 50
 ln2 <- log(2)
 #plot(x, s, type='l')
 
 ######################
-#p <- Mod(fft(s))
-#freqSampling <- 1/sampling
-#f <- freqSampling/2*seq(-1, 1, length.out= count)
-#print(length(p))
-#p1 <- p[1:(length(p)/2)]
-#p2 <- p[(length(p)/2+1):(length(p))]
-#p <- c(p2, p1)
-#print(length(p))
-#print(length(f))
-##p <- p[1:length(f)]
-##plot(f,p, 'l')
+p <- Mod(fft(s))
+freqSampling <- 1/sampling
+f <- freqSampling/2*seq(-1, 1, length.out= count)
+print(length(p))
+p1 <- p[1:(length(p)/2)]
+p2 <- p[(length(p)/2+1):(length(p))]
+p <- c(p2, p1)
+print(length(p))
+print(length(f))
+#p <- p[1:length(f)]
+#plot(f,p, 'l')
 #g <- cohlen * sqrt(pi/ln2) * exp(-pi^2*cohlen^2/ln2*f^2)
-##g <- dnorm(f, 0, 0.1)
-#c <- conv(g,p)
-#print(length(c))
-#f1 <- seq(f[1]*2, f[length(f)]*2, length.out= length(c))
-##plot(f1, c, 'l')
+g <- exp(-pi^2*cohlen^2/ln2*f^2)
+#g <- dnorm(f, 0, 0.1)
+c <- 1-(conv(g,p)/trapz(seq(0, count/2, length.out= length(p)), p))
+print(length(c))
+f1 <- seq(f[1]*2, f[length(f)]*2, length.out= length(c))
 ######################
 
 
@@ -68,6 +68,7 @@ for (tau in taus) {
 }
 intTerm2 <- 2 * trapz(taus, sampledTerm2) #integrate(term2, 0, 3*cohlen, subdivisions=1000)$value
 print(intTerm2)
+print(cohlen*sqrt(pi/ln2))
 for (freq in freqs) {
   
   sampledTerm1 <- c()
@@ -77,6 +78,7 @@ for (freq in freqs) {
   #plot(taus, sampledTerm1, 'l')
   intTerm1 <- 2*trapz(taus, sampledTerm1) #integrate(term1, 0, 3*cohlen, subdivisions=1000)$value
   intNorm <- integrate(function(tau) norm(tau, freq), lower=-Inf, upper=Inf, subdivisions=1000)$value
+  print(paste('num/den:', toString(intTerm1), '/', toString(intNorm)))
   d2 <- c(d2, 1 - (intTerm1 + intTerm2) / intNorm)
   #d2 <- c(d2, intTerm1)
 }
@@ -85,3 +87,4 @@ for (freq in freqs) {
 d2_expected <- read.table(paste('d2_expected_',toString(testDataNo),'_',toString(cohlen),'.csv', sep=''), header = FALSE)
 plot(d2_expected, type='l', col = 'red')
 lines(freqs, d2, col = 'green')
+lines(f1, c, col = 'blue')
